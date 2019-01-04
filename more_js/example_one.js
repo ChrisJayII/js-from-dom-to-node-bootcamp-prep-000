15e9001a8b49b67e442e24620435b38b9898c8a4
Christpher <chrisoutrules@yahoo.com>
2019-01-04 00:13:05 +0000

    Automatically backed up by Learn

--- more_js/example_one.js
+++ more_js/example_one.js
@@ -4,8 +4,3 @@ let person4 = "God"
 console.log("Hi, how are you " + person3 + '?')
 console.log("Oh, I am doing just fine, " + person4 + ", thanks!")
 
-let person1 = "Peter"
-let person2 = "Withnail"
-
-console.log( 'Hi, how are you, ' + person2 + '?' )
-console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
