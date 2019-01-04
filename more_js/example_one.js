5636afd730bd30e6943c080d4688aec632d8acad
Christpher <chrisoutrules@yahoo.com>
2019-01-04 00:12:59 +0000

    Automatically backed up by Learn

--- more_js/example_one.js
+++ more_js/example_one.js
@@ -1,3 +1,9 @@
+let person3 = "Christopher"
+let person4 = "God"
+
+console.log("Hi, how are you " + person3 + '?')
+console.log("Oh, I am doing just fine, " + person4 + ", thanks!")
+
 let person1 = "Peter"
 let person2 = "Withnail"
 
